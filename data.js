f790d71b998fb035dd162dec042237239dfc190d
Tian Tian <tiantian14622105@gmail.com>
2026-09-16 20:17:13 +0800

    Update data.js

--- data.js
+++ data.js
@@ -13,6 +13,14 @@ const SITE_DATA = {
       caption_cn: "我爱你",
       date: "2026/09/16",
       likes: 0
+    },
+     {
+      id: "winter1",
+      url: "uploads/winter1.jpg",
+      caption_en: "Jude says my growth spurt was different from his. He says I suddenly stretched out so long, my shoulders broad enough to rival the pines behind us—but he refuses to say out loud that I’m taller than him now. It really is different. His was the slow fading of cheek softness, a gold-lit smile slowly unfurling under the Dortmund sun, the curls on his head growing fluffier and bouncier by the day. I don’t know what Jude is watching on our video calls. I only notice that he’s been stung a little by the world’s ",
+      caption_cn: "Jude说我的抽条和他的不一样，说我一下子变得好长，肩宽堪比后面的松柏，但拒绝说出我已经比他高的事实。的确不一样，他的是脸颊肉慢慢消掉，在多特蒙德的阳光下慢慢舒展开金灿灿的笑脸，头上的卷卷也慢慢变得更蓬更跳跃。不知道打视频时Jude都在注意什么，我只注意到他受了点世界的不善良，但他选择了继续微笑。",
+      date: "2026/09/16",
+      likes: 0
     }
   ]
 };
