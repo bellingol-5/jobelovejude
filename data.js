3fb59511c2a7d93f460bd7db6103a246b79a6953
Tian Tian <tiantian14622105@gmail.com>
2026-09-16 20:31:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -11,16 +11,32 @@ const SITE_DATA = {
       url: "uploads/26a66fbe-cf83-4ece-9055-d55f97d78f3b.jpg",
       caption_en: "with my brother",
       caption_cn: "我爱你",
-      date: "2026/09/16",
-      likes: 0
+      date: "2020/08/11",
+      likes: 26
     },
      {
       id: "winter1",
       url: "uploads/winter1.jpg",
-      caption_en: "Jude says my growth spurt was different from his. He says I suddenly stretched out so long, my shoulders broad enough to rival the pines behind us—but he refuses to say out loud that I’m taller than him now. It really is different. His was the slow fading of cheek softness, a gold-lit smile slowly unfurling under the Dortmund sun, the curls on his head growing fluffier and bouncier by the day. I don’t know what Jude is watching on our video calls. I only notice that he’s been stung a little by the world’s ",
+      caption_en: "Jude says my growth spurt was different from his. He says I suddenly stretched out so long, my shoulders broad enough to rival the pines behind us—but he refuses to say out loud that I’m taller than him now. It really is different. His was the slow fading of cheek softness, a gold-lit smile slowly unfurling under the Dortmund sun, the curls on his head growing fluffier and bouncier by the day. I don’t know what Jude is watching on our video calls. I only notice that he’s been stung a little by the world’s unkindness—and still, he chose to keep smiling. ",
       caption_cn: "Jude说我的抽条和他的不一样，说我一下子变得好长，肩宽堪比后面的松柏，但拒绝说出我已经比他高的事实。的确不一样，他的是脸颊肉慢慢消掉，在多特蒙德的阳光下慢慢舒展开金灿灿的笑脸，头上的卷卷也慢慢变得更蓬更跳跃。不知道打视频时Jude都在注意什么，我只注意到他受了点世界的不善良，但他选择了继续微笑。",
+      date: "2025/07/18",
+      likes: 30
+    },
+     {
+      id: "winter2",
+      url: "uploads/winter2.jpg",
+      caption_en: "Jude, now so many people are loving me by going around you. It’s hard to discipline the way others love, but I’ll always want every Cupid’s arrow aimed at me to come straight—no turning, no swerving. I’ve been standing behind you in silence all along. Whoever loves me, may they love you too—because I love you.",
+      caption_cn: "Jude，现在有很多人绕过你来爱我了。难以规训别人的爱意，但永远希望那些射向我的丘比特之箭径直而来，不转弯。一直沉默伫立在你身后，爱我的人请同样爱你，因为我爱你。",
+      date: "2026/09/15",
+      likes: 51
+    },
+     {
+      id: "winter3",
+      url: "uploads/winter3.jpg",
+      caption_en: "Useless, every time you show off about Mom being your personal chauffeur, driving you around, that’s exactly how I reply. And still, you never tire of sharing it—unlike your selfies, which always carry a tiny, mischievous kind of half-effort. The photos you take of Mom always catch such lovely light on her face, and she’s smiling toward you behind the camera and toward the “me” you keep murmuring about.I know you’re not really showing off. Mom just misses me a little. And so do you.",
+      caption_cn: "Useless，每次你炫耀妈妈载着你当你的专属司机，我都这样回复。可你还是乐此不疲地分享，不像自拍带着小小恶趣味的敷衍。你拍的妈妈总是有很好看的光影落在她脸上，朝着摄像头后的你和你口中念叨的“我”笑。其实我知道你不是在炫耀，只是妈妈有些想念我，你也是。",
       date: "2026/09/16",
-      likes: 0
+      likes: 32
     }
   ]
 };
